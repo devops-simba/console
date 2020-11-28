dc438a05f5ec6598210299b2e0e5a212eec51795
mohammadmahdi.roozitalab <mohammadmahdi.roozitalab@snapp.cab>
2020-11-28 17:19:35 +0330

    Switched yarn version to 1-22.10

--- fast-build/build.sh
+++ fast-build/build.sh
@@ -1,37 +1,48 @@
 #!/bin/sh
 
-CUR_DIR=`realpath .`
-MY_DIR=`realpath "$(dirname "$0")"`
-ROOT_DIR=`realpath "$MY_DIR/.."`
-YARN_CACHE="/tmp/.yarn-cache"
-#BUILD_DIR="/tmp/console-`cat /proc/sys/kernel/random/uuid`"
-BUILD_DIR="$MY_DIR/.build"
-OUTPUT_DIR="$MY_DIR/.output"
-IMAGE_TAG="mehbos/openshift-console-modified:4.7"
+SCRIPT_DIR=`realpath "$(dirname "$0")"`
+APP_DIR=`realpath "$SCRIPT_DIR/.."`
 
-cd "$MY_DIR"
-mkdir -p $YARN_CACHE
-mkdir -p "$BUILD_DIR"
-rm -rf "$OUTPUT_DIR"
-mkdir -p "$OUTPUT_DIR"
-cp -r "$ROOT_DIR" $BUILD_DIR
+build_application() {
+  YARN_CACHE="/tmp/.yarn-cache"
+  OUTPUT_DIR="$SCRIPT_DIR/.output"
+  IMAGE_NAME="registry.apps.internal.ic.cloud.snapp.ir/openshift-console/openshift-console-modified"
+  IMAGE_TAG="4.8"
 
-# remove files that remained from local builds
-find $BUILD_DIR -type d -name node_modules -exec rm -rf {} \;
+  app_location="$1"
+  mkdir -p "$OUTPUT_DIR"
+  docker run -it --rm \
+      -v "$1:/go/src/github.com/devops-simba/console" \
+      -v "$YARN_CACHE:/usr/local/share/.cache" \
+      quay.io/coreos/tectonic-console-builder:v20 \
+      /go/src/github.com/devops-simba/console/fast-build/init.sh && \
+  cp -r "$1/bin/bridge" "$OUTPUT_DIR" && \
+  cp -r "$1/frontend/public/dist" "$OUTPUT_DIR" && \
+  cp "$1/pkg/graphql/schema.graphql" "$OUTPUT_DIR" && \
+  docker build --rm -t "$IMAGE_NAME:$IMAGE_TAG" -f "$SCRIPT_DIR/Dockerfile" "$OUTPUT_DIR"
+  rm -rf "$OUTPUT_DIR"
+}
 
-CONSOLE_DIR=$BUILD_DIR/console
-docker run -it --rm \
-  -v "$YARN_CACHE:/usr/local/share/.cache" \
-  -v "$CONSOLE_DIR:/go/src/github.com/devops-simba/console" \
-  quay.io/coreos/tectonic-console-builder:v20 \
-  /go/src/github.com/devops-simba/console/fast-build/init.sh && \
-cp $CONSOLE_DIR/bin/bridge "$OUTPUT_DIR" && \
-cp -r $CONSOLE_DIR/frontend/public/dist "$OUTPUT_DIR/dist" && \
-cp $CONSOLE_DIR/pkg/graphql/schema.graphql "$OUTPUT_DIR" && \
-docker build --rm -t "$IMAGE_TAG" -f "$MY_DIR/Dockerfile" "$OUTPUT_DIR"
+USERNAME="$1"
+case "$2" in
+  out|out-of-place)
+    BUILD_LOCATION="/tmp/console-`cat /proc/sys/kernel/random/uuid`"
 
-rm -rf "$OUTPUT_DIR"
-rm -rf $BUILD_DIR
-exit 0
+    echo "out of place build(copy solution and build somewhere else)"
+    # remove old node_modules in place, so we create new ones
+    find "$BUILD_LOCATION" -type d -name node_modules -exec rm -rf {} +
+    cp -r "$APP_DIR" "$BUILD_LOCATION"
+    build_application "$BUILD_LOCATION"
+    rm -rf "$BUILD_LOCATION"
+  ;;
 
-cd "$CUR_DIR"
+  *)
+    echo "in place build(build inside the solution, will create some data in the solution)"
+    build_application "$APP_DIR"
+  ;;
+esac
+
+echo "Pushing image to image registry"
+docker login "registry.apps.internal.ic.cloud.snapp.ir" -u "${USERNAME:-mohammadmahdi.roozitalab}" -p `oc whoami -t`
+docker push "$IMAGE_NAME:$IMAGE_TAG"
+
